6bdf1c167bdcbcfe42f83c26ec6899dc13ce065f
joverdejo <joverdejo@gmail.com>
2021-05-12 02:27:22 -0400

    putting server back to working condition

--- src/Server/index.js
+++ src/Server/index.js
@@ -1,14 +1,17 @@
 const db = {};
 const express = require('express');
-var cors = require('cors')
-var app = express()
- 
+const app = express();
 var bodyParser = require("body-parser");
 
-app.use(cors())
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 app.post('/jovirtual/coords', respond)
 var port = process.env.PORT || 8080;
 
