2174d5fa96544d1293dfdd952dd7d36b0355b5fa
joverdejo <joverdejo@gmail.com>
2021-05-12 02:10:45 -0400

    info for server

--- src/Server/index.js
+++ src/Server/index.js
@@ -7,11 +7,11 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://jovirtual.herokuapp.com"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
-
 app.post('/jovirtual/coords', respond)
 var port = process.env.PORT || 8080;
 
