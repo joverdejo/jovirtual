f1dba327b1bfc284eda0a35299cda5f3b48e6f5c
joverde <joverdejo@gmail.com>
2021-05-20 02:58:52 -0400

    trying sockets server

--- src/Server/index.js
+++ src/Server/index.js
@@ -6,7 +6,7 @@ var bodyParser = require("body-parser");
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: {
-    origin: "http://localhost:3000"||"https://jovirtual.herokuapp.com",
+    origin: false,
     methods: ["GET", "POST", "HEAD", "OPTIONS", "PUT"]
   }
 });
